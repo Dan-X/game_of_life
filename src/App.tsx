be977b51f97c331b5e240816449d38e084363929
Daniel <19605082+Dan-X@users.noreply.github.com>
2021-07-19 15:18:12 +0200

    add pataternLib

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react"
+
+
 import Board from "./Board/Board";
 
-import "@blueprintjs/core/lib/css/blueprint.css";
-
+// import "@blueprintjs/core/lib/css/blueprint.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <div className="header">
-        <H1>Conway's Game of Life</H1>
-      </div> */}
-      <div className="mainContainer">
-        <Board />
-      </div>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        {/* <div className="header">
+          <H1>Conway's Game of Life</H1>
+        </div> */}
+        <div className="mainContainer">
+          <Board />
+        </div>
+      </div>  
+    </ChakraProvider>
+    
   );
 }
 
