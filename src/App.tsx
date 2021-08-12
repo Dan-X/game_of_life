f40005736cd0cfb5ad8394f28edf8603862261a5
Daniel <19605082+Dan-X@users.noreply.github.com>
2021-08-12 13:49:06 +0200

    add links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react"
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
 
 import Board from "./Board/Board";
 
@@ -12,10 +12,22 @@ function App() {
   return (
     <ChakraProvider>
       <div className="App">
-          <Board />
-      </div>  
+        <Board />
+        <div className="linkGroup">
+          <div className="linkBtn">
+            <Link href="https://github.com/Dan-X/game_of_life" isExternal>
+              Github
+            </Link>
+          </div>
+
+          <div className="linkBtn">
+            <Link href="https://xinshi.me" isExternal>
+              My site
+            </Link>
+          </div>
+        </div>
+      </div>
     </ChakraProvider>
-    
   );
 }
 
